3f2a005144ff65008be66416753ac53363343bea
Chris Myers <cm1220@ic.ac.uk>
2022-08-02 21:57:21 +0100

    Added 6

---
+++ 6_sumsquaredifference.cpp
@@ -0,0 +1,10 @@
+// Find the difference between the sum of the squares of the first one hundred natural numbersand the square of the sum.
+
+#include <iostream>
+
+int main() {
+	int result = 50 * 101 * 67 - 50 * 50 * 101 * 101;
+
+	std::cout << result << std::endl;
+	return 0;
+}
